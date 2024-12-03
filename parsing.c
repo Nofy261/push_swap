e83c8c5a922da35c9951ea9ffc549826c6059596
administrateur <administrateur@MacBook-Air-de-administrateur.local>
2024-12-03 16:54:01 +0100

    modif parsing

--- parsing.c
+++ parsing.c
@@ -19,47 +19,58 @@ int	ft_isdigit(int c)
 	return (0);
 }
 //creer une fonction qui parcourt un tableau de tableau 
-int	check_sign(int argc, char **argv)
+int	check_sign(char **argv)
 {
 	int	i;
 	int	j;
 
 	i = 0;
 	j = 1;
-	while (j < argc)
+	while (argv[j])
 	{
+		i = 0;
 		while(argv[j][i])
 		{
-			if((argv[j][0] == '-' || argv[j][0] == '+') && (argv[j][i + 1] == ft_isdigit))
-				return (1);//on return un booleen pour dire vrai // le return n'arrete-t'il pas la fonction??? 
-			else if((argv[j][0] == '-' || argv[j][0] == '+') && (argv[j][i + 1] != ft_isdigit))
-				return (0);
-			else if (argv[j][0] == 32 && (argv[j][i + 1] == 32 || argv[j][i + 1] == '\0'))
+			if(argv[j][0] == '-' || argv[j][0] == '+')
 			{
-				write (1, "Error\n", 6);
-				return (0);
+				if(!ft_isdigit(argv[j][i + 1]))
+					return (1);
 			}
+			if (argv[j][0] == 32 && (argv[j][1] == 32 || argv[j][1] == '\0'))
+				return (0);
 			i++;//on va checker les autres chiffres/caractere?
 		}
-		j++;
 	}
 	if (i == 0)
 		return (1);
-	return(0);//??
+	j++;
+	return(0);
 }
 
 //boucle qui parcourt argc
 //fonction qui stocke argv
 //split sur les espaces
 //creer un tableau de tableau et stocke la valeur de retour de ft_split
-function()
+int	main(int argc, char **argv)
 {
 	int	i;
 
 	i = 1;
-	while (argc[i])
+	char **tableau;
+	t_list *list_a;
+	t_list *list_b;
+
+	list_a = NULL;
+	list_b = NULL;
+	if (verif_argc(argc) == 1 || check_sign(argv) == 1)
 	{
-		tableau = ft_split(argc);
+		ft_putstr_fd("Error\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	while (i < argv)
+	{
+		tableau = ft_split(argv[i], ' ');
+		
 	}
 
 
