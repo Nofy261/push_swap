c62b70dbf18c279f04f557405219c66264dc7294
Nofy Leclercq <nolecler@3F1.42angouleme.fr>
2024-12-06 16:55:23 +0100

    main.c modif

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 15:35:20 by nolecler          #+#    #+#             */
-/*   Updated: 2024/12/06 12:12:13 by nolecler         ###   ########.fr       */
+/*   Updated: 2024/12/06 16:20:38 by nolecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,34 @@
 
 // gerer int min et int max ❌ 
 
-// fonction pour voir si il est deja trier  int	is_sorted(t_list *stack)  ❌ 
+// fonction pour voir si il est deja trier  int	is_sorted(t_list *stack)  ✅ 
 // appel ft_lstsize pour voir le nombre de node que contient la liste chainee 
 // et stock le retour dans un int ❌ 
 // Si il y a que deux chiffres fais une fonction qui fais un trie par deux (swap)
 
 #include <stdio.h>
 
-//dans la pile a , il y a des nodes, je dois verifier si les nodes sont trier ou pas
-//fonction qui compare les nodes, 
+// = 1 veut dire que ce n'est pas triee -> Donc creer une fonction qui va trier = ALGORITHME ❌
+// = 0 veut dire que c'est triee
+
+//Si aucun paramètre n’est spécifié, le programme ne doit rien afficher et rendre l’invite de commande.(cf sujet) 
 
 
 
-
-
-
-
+int	is_sorted(t_list *stack_a)
+{
+	while (stack_a->next != NULL)
+	{		
+		if (stack_a->value > stack_a->next->value)
+		{
+			printf("ce n'est pas triee");
+			return (1);
+		}
+		stack_a = stack_a->next;
+	}
+	printf("c'est deja triee");
+	return (0);	
+}
 
 int	main(int argc, char **argv)
 {
@@ -56,6 +68,7 @@ int	main(int argc, char **argv)
 		i++;
 	}
 	check_double(&stack_a);
+	is_sorted(stack_a);
 }
 
 
