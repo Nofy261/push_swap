7fd9cb5908c37ee50573458643ce39622e501275
Nofy Leclercq <nolecler@3G4.42angouleme.fr>
2024-12-10 13:00:37 +0100

    find min, max

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 15:35:20 by nolecler          #+#    #+#             */
-/*   Updated: 2024/12/07 15:17:19 by nolecler         ###   ########.fr       */
+/*   Updated: 2024/12/10 12:55:11 by nolecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,6 @@
 
 #include <stdio.h>
 
-// = 1 veut dire que ce n'est pas triee -> Donc creer une fonction qui va trier = ALGORITHME ❌
-// = 0 veut dire que c'est triee
-
-//Si aucun paramètre n’est spécifié, le programme ne doit rien afficher et rendre l’invite de commande.(cf sujet) 
-
-int	is_sorted(t_list *stack_a)
-{
-	while (stack_a->next != NULL)
-	{		
-		if (stack_a->value > stack_a->next->value)
-		{
-			printf("ce n'est pas triee");
-			return (1);
-		}
-		stack_a = stack_a->next;
-	}
-	printf("c'est deja triee");
-	return (0);	
-}
 
 void	sort_2(t_list *stack_a)
 {
@@ -50,7 +31,7 @@ void	sort_2(t_list *stack_a)
 	if (i == 2)
 		swap_nodes(&stack_a, 'a');
 }
-//creer une fonction qui determine qui est le plus grand des nodes
+//creer une fonction qui determine qui est le plus grand des nodes  ✅ 
 
 // Définir une variable max = premier élément de la pile.
 // Tant qu'il reste des éléments dans la pile :
@@ -66,46 +47,89 @@ t_list    *find_max(t_list *stack_a)
 	
 	while(stack_a)
 	{
-		if(stack_a->value < stack_a->next->value)
-			max = stack_a->next;
+		if(stack_a->value > max->value)
+			max = stack_a;
 		stack_a = stack_a->next;
 	}
 	return (max);
 }
-
-
-
-
+t_list    *find_min(t_list *stack_a)
+{
+	t_list *min = stack_a; //les deux pointent vers un meme maillon
+	//t_list *min = *stack_a; = acces a la valeur du maillon pointee par stack_a
+	
+	while(stack_a)
+	{
+		if(stack_a->value < min->value)
+			min = stack_a;
+		stack_a = stack_a->next;
+	}
+	return (min);
+}
 
 //creer une fonction qui trie 3nombres
-void	sort_3(t_list **stack_a)
+void	sort_3(t_list *stack_a)
 {
 	int	i;
-	int	position;
-	
-	position = 0;// ou 1??
+	t_list	*min;
+	t_list	*max;
+
+	min = find_min(stack_a);
+	max = find_max(stack_a);
 	i = ft_lstsize(stack_a);
-	t_list	*node = *stack_a;
-	
+	//t_list	*node = stack_a; //node et stack_a pointe vers le meme maillon
+
+	//Un pointeur qui pointe vers le premier element de la liste
+	//un pointeur qui pointe vers le max
+	//un pointeur qui pointe le min
+
 	if (i == 3)
 	{
-		//si node2 est en debut de la liste et node3 a la fin de la liste
-		//--> "sa"
-
-		// si node3 est au debut et node1 a la fin 
-		//--> "sa" puis "rra"
-
-		//si node3 au debut et node2 a la fin
-		// --> "ra"
+		//si max est a la fin et min au milieu : sa
 		
-		//si node1 au debut et node2 a la fin
-		//--> "sa" puis "ra"
 
-		//si node1 a la fin et node2 au debut
-		//--> "rra"
+		//si max au debut et min a la fin : sa puis rra
+		
+
+		//si max au debut et min au milieu : ra
+		//if (stack_a->value == max->value && stack_a->next->value == min->value)
+		//	rotate(&stack_a, 'a');
+					
+		
+		
+		//si min au debut et max au milieu : sa puis ra
+		if (stack_a->value == min->value && stack_a->next->value == max->value)
+		{
+			swap_nodes(&stack_a, 'a');
+			rotate(&stack_a, 'a');
+		}
+
+		//si max au milieu et min a la fin : rra
+		
 	}
-	
+}
 
+
+// = 1 veut dire que ce n'est pas triee -> Donc creer une fonction qui va trier = ALGORITHME ❌
+// = 0 veut dire que c'est triee
+
+//Si aucun paramètre n’est spécifié, le programme ne doit rien afficher et rendre l’invite de commande.(cf sujet) 
+
+int	is_sorted(t_list *stack_a)
+{
+	while (stack_a->next != NULL)
+	{		
+		if (stack_a->value > stack_a->next->value)
+		{
+			printf("ce n'est pas triee\n");
+			sort_2(stack_a);
+			sort_3(stack_a);
+			return (1);
+		}
+		stack_a = stack_a->next;
+	}
+	printf("c'est deja triee");
+	return (0);	
 }
 
 int	main(int argc, char **argv)
@@ -135,6 +159,7 @@ int	main(int argc, char **argv)
 	if (is_sorted(stack_a) == 1)
 	{
 		sort_2(stack_a);
+		sort_3(stack_a);
 	}
 }
 
